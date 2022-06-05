54b9b7161567562fb3f555773d24e09dc47b9bdf
Joe Cardenas <coderjoe05@gmail.com>
2022-06-05 16:42:50 -0400

    Icon takes you home

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,7 +5,7 @@ import thumb from "./../Media/thumb.png"
 const Header: Component<{page: String}> = (props) => {
     return (
         <header class="flex flex-row p-2 gap-3 h-16 w-screen items-center text-xl">
-            <img src={thumb} alt="" class="h-full rounded-md"/>
+            <a href="/" class="h-full aspect-square"><img src={thumb} alt="" class="rounded-md"/></a>
             <a class={`flex flex-col justify-center p-2 h-full rounded-md border border-white hover:border-black transition-all duration-200 ${props.page == "home" ? "bg-slate-200" : ""}`} href="/">Home</a>
             <a class={`flex flex-col justify-center p-2 h-full rounded-md border border-white hover:border-black transition-all duration-200 ${props.page == "support" ? "bg-slate-200" : ""}`} href="/support">Support</a>
       </header>
